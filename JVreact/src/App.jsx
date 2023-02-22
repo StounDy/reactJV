1db6941cca776bfc152aec399bae9be9d182335d
EstebanGutierrezGarcia <raisings1978@hotmail.com>
2023-02-21 19:21:22 -0600

    jvreact

---
+++ JVreact/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    // is a React.Fragment <> </>
+    <div className='container'>
+      <h1>Hola mundo</h1>
+      <h2>Hola ...</h2>
+    </div>
+  )
+}
+
+export default App
