8f7842f223ef6e00e8f47126ceb10f1e0ea38397
EstebanGutierrezGarcia <raisings1978@hotmail.com>
2023-03-15 00:58:58 -0600

    productos¿

--- JVreact/src/App.jsx
+++ JVreact/src/App.jsx
@@ -1,16 +1,33 @@
 import { useState,} from 'react'
 import  Navbar from './assets/components/Navbar'
 import './App.css'
+import ProductList from './assets/components/ProductList';
 
 
 function App() {
 
   return (
     <>
-    <h1>lista de productos</h1>
+    <h2>lista de productos</h2>
     <Navbar/>
-    </>
-      
+   
+    <Header
+				allProducts={allProducts}
+				setAllProducts={setAllProducts}
+				total={total}
+				setTotal={setTotal}
+				countProducts={countProducts}
+				setCountProducts={setCountProducts}
+			/>
+			<ProductList
+				allProducts={allProducts}
+				setAllProducts={setAllProducts}
+				total={total}
+				setTotal={setTotal}
+				countProducts={countProducts}
+				setCountProducts={setCountProducts}
+			/>
+		</>
  );
   
 }
